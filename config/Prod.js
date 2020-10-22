046204fa196fca851040d9117b07bd4ec4f5da06
Krzysztof Wolny <krzysztof.wolny@outlook.com>
2020-10-22 09:02:35 +0200

    publishable hey hardcoded in prod.js. Need to be changed

--- config/Prod.js
+++ config/Prod.js
@@ -3,6 +3,6 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
-    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+    stripePublishableKey: 'pk_test_51HdxalBUW5aoHdp3XelNXMmlG5nfZ9y48JgP1nMfQMcn88gojV8y8UH7cvvIBvnd1mk83T2gdXLFlYZV16noyQim00EGMj3Wfb',
+    stripeSecretKey: 'sk_test_51HdxalBUW5aoHdp3uyqud3UM9bcnEvGtqeLSWZOEdNBN17bYjBwU8oKN8C0yqoaTxQGqqlqJDMubjnqk094rgKBc00Sd09Pblp'
 };
